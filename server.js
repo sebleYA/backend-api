b373c85c263f492692af659e1247b6be3cdacf51
sebli <seblewongelabera@gmail.com>
2022-04-15 11:39:14 -0700

    function added to handle rejection while connecting to db

--- server.js
+++ server.js
@@ -1,25 +1,35 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Route files
-const bootcamps = require('./routes/bootcamps');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // load env vars
 dotenv.config({ path: './config/config.env' });
 
+// connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
 
-
-// dev logging middleware
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'));
+// dev logging middlewares
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
-
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 6000;
 
-app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+ 
+// Handle unhandled promise Rejection
+process.on('unhandledRejection', (err, Promise) =>{
+    console.log(`Error: ${err.message}`.red.bold);
+    // close server & exit process
+    server.close(() => process.exit(1)) // exit the process with failer and crash the server
+})
