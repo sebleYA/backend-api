15875361819b233c133b512d1f3c872f2a2cc7ce
sebli <seblewongelabera@gmail.com>
2022-04-26 15:04:13 -0700

    added error handler middlware

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
+const errorHandler = require('./middlerware/error');
 const connectDB = require('./config/db');
 
 // load env vars
@@ -24,6 +25,10 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+
+// erro handling
+app.use(errorHandler);
+
 
 const PORT = process.env.PORT || 6000;
 
