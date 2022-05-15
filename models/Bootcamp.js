a09dec9ff3f8f48189a06cf2b268621270722a45
sebli <seblewongelabera@gmail.com>
2022-05-14 20:41:27 -0700

    updated by adding address and location using node geocoder

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const slugify = require ('slugify')
+const slugify = require ('slugify');
+const geocoder = require('../utils/geocoder')
 // create Bootcamp Schema
 const BootcampSchema = new mongoose.Schema({
     name: {
@@ -101,6 +102,25 @@ BootcampSchema.pre('save', function(next){
     next()
 });
 
+
+// Geocode and  create location field
+
+BootcampSchema.pre('save', async function(next){
+    const loc = await geocoder.geocode(this.address);
+    this.location = {
+        type :'point',
+        coordinates: [loc[0].longitude, loc[0].latitude],
+        formattedAddress: loc[0].formattedAddress,
+        street: loc[0].streetName,
+        city: loc[0].city,
+        state: loc[0].stateCode,
+        zipcode: loc[0].zipcode,
+        country: loc[0].countryCode,
+    }
+    // Dont save address in DB
+    this.address = undefined;
+    next();
+})
 
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema)
