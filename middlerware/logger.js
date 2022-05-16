e6c9206ecc3d1cbf7c6bad422f46213397a1566b
sebli <seblewongelabera@gmail.com>
2022-05-15 23:15:15 -0700

    deleted uneccesary file

--- middlerware/logger.js
+++
@@ -1,7 +0,0 @@
-//  @desc       Logs request to console
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.protocol}://${req.get('host')}${req.url}`);
-  next();
-};
-
-module.exports = logger;
