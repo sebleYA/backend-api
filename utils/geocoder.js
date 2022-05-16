a238d446ca4c8ac74e87aa432a4b6836f2457023
sebli <seblewongelabera@gmail.com>
2022-05-15 21:57:42 -0700

    insert proper name for option object

--- utils/geocoder.js
+++ utils/geocoder.js
@@ -1,10 +1,10 @@
-const NodeGeocoder = require ('node-geocoder');
+const NodeGeocoder = require('node-geocoder');
 
 const options = {
-    provider : process.env.GEOCODER_PROVIDER,
-    httpadapter : 'https',
-    apikey: process.env.GEOCODER_API_KEY,
-    formatter : null
+  provider: process.env.GEOCODER_PROVIDER,
+  httpAdapter: 'https',
+  apiKey: process.env.GEOCODER_API_KEY,
+  formatter: null
 }
 
 const geocoder = NodeGeocoder(options);
