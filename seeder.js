6b364e35edc73425711c3eb69891be6299864ebe
sebli <seblewongelabera@gmail.com>
2022-05-16 20:40:09 -0700

    seeder for courses

--- seeder.js
+++ seeder.js
@@ -8,19 +8,17 @@ dotenv.config({ path: './config/config.env' });
 
 // Load models
 const Bootcamp = require('./models/Bootcamp');
-const Course = require('./models/Course');
+
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI);
 
 // Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`,'utf-8'));
-const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`,'utf-8'));
 
 //Import into DB
 const importData = async() =>{
     try {
         await Bootcamp.create(bootcamps);
-        await Course.create(courses);
         console.log('Data Imported... ');
         process.exit()
     } catch (err) {
@@ -33,7 +31,6 @@ const importData = async() =>{
 const deleteData = async() =>{
     try {
         await Bootcamp.deleteMany();
-        await Course.deleteMany();
         console.log('Data Destroyed... ')
         process.exit()
     } catch (err) {
