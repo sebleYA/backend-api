e6f0f3c2b5b43b793a33e26cf171839b6ace9b22
sebli <seblewongelabera@gmail.com>
2022-05-19 22:36:30 -0700

    added protect and authorized middleware route

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -18,15 +18,17 @@ const advancedResults = require('../middlerware/advancedResults');
 
 const router = express.Router();
 
+const { protect , authorize} = require('../middlerware/auth');
+
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
 
 router.route('/radius/:zipcode/:distance').get(getBootcampsInradius);
 
-router.route('/:id/photo').put(bootcampPhotoUpload);
+router.route('/:id/photo').put(protect, authorize('publisher', 'admin'), bootcampPhotoUpload);
 
-router.route('/').get(advancedResults(Bootcamp, 'courses'), getBootcamps).post(createBootcamp);
+router.route('/').get(advancedResults(Bootcamp, 'courses'), getBootcamps).post(protect, authorize('publisher', 'admin'),createBootcamp);
 
-router.route('/:id').get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
+router.route('/:id').get(getBootcamp).put(protect, authorize('publisher', 'admin'), updateBootcamp).delete(protect, authorize('publisher', 'admin'), deleteBootcamp);
 
 module.exports = router;
