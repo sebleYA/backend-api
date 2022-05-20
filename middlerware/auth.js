139eb1f3c59f1e79b09d74946eae4d6761b1de58
sebli <seblewongelabera@gmail.com>
2022-05-19 22:31:06 -0700

    created middleware for auth.js.

---
+++ middlerware/auth.js
@@ -0,0 +1,47 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('./async');
+const errorHandler = require('../utils/errorResponse');
+const User = require('../models/User');
+const ErrorResponse = require('../utils/errorResponse');
+
+// Protect routes
+exports.protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  //   else if(req.cookies.token){
+  //       token = req.cookies.token
+  //   }
+
+  // Make sure token exists
+  if (!token) {
+    return next(new ErrorResponse('Not authorize to acces this route', 401));
+  }
+
+  try {
+    // Veriy token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    console.log(decoded);
+
+    req.user = await User.findById(decoded.id);
+
+    next();
+  } catch (error) {
+    return next(new ErrorResponse('Not authorize to acces this route', 401));
+  }
+});
+
+
+// Grant access to specific roles
+exports.authorize = (...roles) => {
+    return (req, res, next) => {
+        if(!roles.includes(req.user.role)){
+            return next(new ErrorResponse(`User role ${req.user.role} is not authorized to access`, 403));
+        }
+        next()
+    };
+}
