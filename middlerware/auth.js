928997b600a5a4941cf100f65549ec6ac43f0013
sebli <seblewongelabera@gmail.com>
2022-05-21 18:35:57 -0700

    clear console log

--- middlerware/auth.js
+++ middlerware/auth.js
@@ -25,8 +25,6 @@ exports.protect = asyncHandler(async (req, res, next) => {
     // Veriy token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-    console.log(decoded);
-
     req.user = await User.findById(decoded.id);
 
     next();
@@ -35,13 +33,12 @@ exports.protect = asyncHandler(async (req, res, next) => {
   }
 });
 
-
 // Grant access to specific roles
 exports.authorize = (...roles) => {
-    return (req, res, next) => {
-        if(!roles.includes(req.user.role)){
-            return next(new ErrorResponse(`User role ${req.user.role} is not authorized to access`, 403));
-        }
-        next()
-    };
-}
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(new ErrorResponse(`User role ${req.user.role} is not authorized to access`, 403));
+    }
+    next();
+  };
+};
