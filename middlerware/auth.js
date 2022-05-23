76d156084be885f81d87f732a518f4f3852f1649
sebli <seblewongelabera@gmail.com>
2022-05-23 11:01:20 -0700

    updated error message

--- middlerware/auth.js
+++ middlerware/auth.js
@@ -37,7 +37,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
 exports.authorize = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
-      return next(new ErrorResponse(`User role ${req.user.role} is not authorized to access`, 403));
+      return next(new ErrorResponse(`User role ${req.user.role} is not authorized to access this route`, 403));
     }
     next();
   };
