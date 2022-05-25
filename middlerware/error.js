e302622aa0a17d4f8b069e801910d9d378f66c6f
sebli <seblewongelabera@gmail.com>
2022-05-24 21:44:10 -0700

    lint fix

--- middlerware/error.js
+++ middlerware/error.js
@@ -1,34 +1,34 @@
-const ErrorResponse = require("../utils/errorResponse");
+const ErrorResponse = require('../utils/errorResponse');
 
 const errorHandler = (err, req, res, next) => {
-    let error = { ...err}
-    error.message = err.message;
+  let error = { ...err };
+  error.message = err.message;
 
-    // Log to console
-    console.log(err);
+  // Log to console
+  console.log(err);
 
-    // Mongoose bad ObjectId
-    if(err.name === 'CastError'){
-        const message = `Resource not found`;
-        error = new ErrorResponse(message, 404);
-    }
-    
-    // Mongoose Duplicate Key
-    if(err.code === 11000) {
-        const message = 'Duplicate field value entred!'
-        error = new ErrorResponse(message, 400);
-    }
+  // Mongoose bad ObjectId
+  if (err.name === 'CastError') {
+    const message = `Resource not found`;
+    error = new ErrorResponse(message, 404);
+  }
 
-    // Mongoose validation error
-    if(err.name === 'ValidationError'){
-        const message = Object.values(err.errors).map(val => val.message);
-        error = new ErrorResponse(message, 400)
-    }
+  // Mongoose Duplicate Key
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entred!';
+    error = new ErrorResponse(message, 400);
+  }
 
-    res.status(error.statusCode || 500).json({
-        sucess: false,
-        error: error.message || 'Server Error'
-    });
-}
+  // Mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
+  res.status(error.statusCode || 500).json({
+    sucess: false,
+    error: error.message || 'Server Error'
+  });
+};
 
 module.exports = errorHandler;
